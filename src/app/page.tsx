621adb264032e5e35d29c92e25a14cf055c68dfd
AugieAud <auds@missionreadyhq.com>
2025-05-08 12:50:07 +1200

    remove animation from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import Navbar from "../app/components/Navbar";
-import ParticleBackground from "../app/components/ParticleBackground";
 import { TypeAnimation } from "react-type-animation";
 
 export default function Home() {
   return (
     <main className="flex h-screen relative overflow-hidden">
-      <ParticleBackground />
       <Navbar />
 
       {/* Main Content */}
@@ -15,12 +13,12 @@ export default function Home() {
       <section className="fixed bottom-0 right-0 p-8 flex items-end justify-end">
         <TypeAnimation
           sequence={[
-            'I believe software can be thoughtful and playful.',
+            "I believe software can be thoughtful and playful.",
             1000,
-            'I believe software can be thoughtful and playful.\nWith a passion for clean design and creative code,',
+            "I believe software can be thoughtful and playful.\nWith a passion for clean design and creative code,",
             1000,
-            'I believe software can be thoughtful and playful.\nWith a passion for clean design and creative code,\nI craft experiences that are both useful and inspiring.',
-            1000
+            "I believe software can be thoughtful and playful.\nWith a passion for clean design and creative code,\nI craft experiences that are both useful and inspiring.",
+            1000,
           ]}
           wrapper="p"
           cursor={true}
